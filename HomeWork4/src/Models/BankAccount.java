e4c63d10e7a1336300f585be0ec62ea8987d6dcf
MichaelKuch26 <mickey22121989@gmail.com>
2023-08-07 20:27:30 +0400

    Home Work 4

---
+++ HomeWork4/src/Models/BankAccount.java
@@ -0,0 +1,30 @@
+package Models;
+
+/**
+ * Модель банковского аккаунта
+ */
+public class BankAccount {
+  private long card;
+  private static long oldCard;
+  private int balance;
+
+    public BankAccount() {
+        this.card = oldCard + 1;
+        oldCard = this.card;
+        balance = 1000;
+    }
+
+    public void setBalance(int balance) {
+      this.balance = balance;
+    }
+
+    public long getCard() {
+      return card;
+    }
+
+    public int getBalance() {
+      return balance;
+    }
+
+    
+}
